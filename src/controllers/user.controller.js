a1c1f5791d9965073f2e08e1d1234d90e13b2fd6
Sachin Alam <sachinalam4216@gmail.com>
2025-01-04 17:14:14 +0530

    Series ended at bad note.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,8 +170,28 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  User.findById();
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new APIResponse(200, {}, "User Logged out sucessfully!"));
 });
 
-export { loginUser, registerUser };
- 
+export { loginUser, registerUser, logoutUser };
